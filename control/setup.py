e92c6492e318633c3d819f40c45d70979913a803
Sophia Schiffer <mbox.sophia@gmail.com>
2023-11-17 10:29:37 -0600

    launch file for gun scan

--- control/setup.py
+++ control/setup.py
@@ -9,7 +9,8 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml', 'config/tags.yaml']),
+        ('share/' + package_name, ['package.xml', 'config/tags.yaml',
+                                   'launch/shoot_pins.launch.xml']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
