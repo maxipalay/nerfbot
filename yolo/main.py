eebfea4cdb1b48834c792929dcd25b6cf21b6efc
Rahul Roy <rahulroy2909@gmail.com>
2023-11-21 11:59:59 -0600

    removed extra yolo"

--- yolo/main.py
+++
@@ -1,8 +0,0 @@
-from ultralytics import YOLO
-
-# Load a model
-model = YOLO("yolov8n.yaml")  # build a new model from scratch
- # load a pretrained model (recommended for training)
-
-# Use the model
-model.train(data="config.yaml", epochs=800)  # train the model
