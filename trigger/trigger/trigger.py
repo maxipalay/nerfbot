cd219deac3c22bf5cbe22664a5821abb64deccc3
Maximiliano Palay <maximilianopalay@gmail.com>
2023-12-05 16:39:17 -0600

    Added docstrings to trigger node

--- trigger/trigger/trigger.py
+++ trigger/trigger/trigger.py
@@ -1,3 +1,15 @@
+"""
+Node the interacts with an Arduino through serial communication.
+This node is specifically designed for th eNerf Gun project, and
+it is a bridge to ask the arduino to fire a Nerf gun.
+
+Services (offered)
+------------------
+    - /fire (trigger_interfaces/srv/Fire) - Request a gun to be fired
+
+
+"""
+
 import rclpy
 from rclpy.node import Node
 import serial
@@ -8,18 +20,24 @@ class Trigger(Node):
     def __init__(self):
         super().__init__("trigger")
 
+        # we're identifying the Arudino by its serial number
         ARDUINO_SERIAL = "85439313130351612011"
-        ports = list(list_ports.comports(True))
+
+        # ports discovery
+        ports = list(list_ports.comports(True)) # get list of ports
         arduino_port = None
 
+        # grab the port that's connected to the Arduino
         for p in ports:
             if p.serial_number == ARDUINO_SERIAL:
                 arduino_port = p.device
                 break
 
-        if  arduino_port is None:
+        # check Arduino has been found
+        if arduino_port is None:
             print("error finding arduino")
 
+        # create the serial connection
         self.serial_connection = serial.Serial(arduino_port)
 
         # create services
@@ -28,11 +46,26 @@ class Trigger(Node):
 
 
     def fire_callback(self, request, response):
+        """
+        Fire Nerf gun.
+
+        Args
+        ----
+            - self: instance of this class
+            - request: service request
+            - response: service response
+
+        """
         gun_id = request.gun_id
         self.get_logger().info(f"Received request to fire gun with id {gun_id}")
+        
+        # send information to Arduino
         self.serial_connection.write(f"{gun_id}\n".encode(encoding='us-ascii'))
+        
+        # wait for Arduino response
         raw_line = self.serial_connection.readline()
         self.get_logger().info("Received confirmation from arduino")
+        
         return response
 
 def main(args=None):
