8313f71e3e890d1510313f1ac73877540d477f21
Stella <jialuyu2024@u.northwestern.edu>
2023-12-05 20:11:55 -0600

    arduino documented

--- arduino/trigger/trigger.ino
+++ arduino/trigger/trigger.ino
@@ -1,4 +1,14 @@
+/**
+ * @file gun_control.ino
+ * @brief Arduino code for controlling two guns through a serial interface.
+ * @author Max
+ * @date 12/1/2023
+ */
 
+/**
+ * @struct gun_pins
+ * @brief Structure to represent the pin configuration for a gun.
+ */
 struct gun_pins {
   int rev_pin;
   int fire_pin;
@@ -9,6 +19,10 @@ gun_pins gun1 = {4,5};
 
 gun_pins guns[] = {gun0, gun1};
 
+/**
+ * @brief Setup function executed once when the Arduino starts.
+ * Initializes serial communication and sets up pin modes.
+ */
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -18,6 +32,10 @@ void setup() {
   pinMode(5,OUTPUT);
 }
 
+/**
+ * @brief Main loop function executed continuously.
+ * Listens for incoming serial data to control the guns.
+ */
 void loop() {
   while (Serial.available() > 0) {
     // look for the next valid integer in the incoming serial stream:
